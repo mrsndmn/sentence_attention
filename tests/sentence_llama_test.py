687447b44db197c666aeeda1feb9f2c7ab55840c
Dmitrii Tarasov <d.tarasov@airi.net>
2025-08-06 22:42:06 +0300

    add tokenization tests

--- tests/sentence_llama_test.py
+++ tests/sentence_llama_test.py
@@ -108,8 +108,6 @@ def test_sentence_llama_model_generate_with_eos_token_and_attention_mask_partial
     print(f"Resized model embeddings to vocabulary size: {len(tokenizer)}")
     model.config.end_of_sentence_token_id = tokenizer.convert_tokens_to_ids('<end_of_sentence>')
 
-    # model.config._attn_implementation = 'eager'
-    # model.config._attn_implementation = 'sdpa'
     model.config._attn_implementation = 'sentence_attention'
 
     input_ids = tokenizer.encode("Russia - Moscow. France - Paris. Germany - Berlin. Italy - ", return_tensors="pt")
@@ -144,114 +142,6 @@ def test_sentence_llama_model_generate_with_eos_token_and_attention_mask_partial
     logits_diff = (output1.logits[:, :seq_len // 2, :] - output2.logits).norm()
     assert logits_diff < 0.04, "logits diff is low"
     assert torch.allclose(output1.logits[:, :seq_len // 2, :], output2.logits, atol=1e-2), "logits are not equal"
-
-
-
-def test_sentence_attention_attention_mask():
-
-    hidden_size = 128
-    batch_size = 4
-    seq_len = 7
-    num_heads = 8
-
-    scaling = 1.0 / hidden_size ** 0.5
-
-    q = torch.rand(batch_size, num_heads, seq_len, hidden_size)
-    k = torch.rand(batch_size, num_heads, seq_len, hidden_size)
-    v = torch.rand(batch_size, num_heads, seq_len, hidden_size)
-    attention_mask = torch.ones(batch_size, 1, seq_len, seq_len)
-
-    special_embeddings_mask = torch.zeros(batch_size, seq_len, dtype=torch.long)
-    special_embeddings_mask[:, 2] = 1
-    special_embeddings_mask[:, 5] = 1
-    clothest_end_of_sentence_token_idx = special_token_mask_to_clothest_token_idx_slow(special_embeddings_mask)
-
-    output1, _ = sentence_attention_forward(
-        None,
-        q, k, v,
-        attention_mask,
-        scaling=scaling,
-        clothest_end_of_sentence_token_idx=clothest_end_of_sentence_token_idx,
-        special_embeddings_mask=special_embeddings_mask,
-    )
-
-    seq_len2 = seq_len * 2
-    q2 = torch.rand(batch_size, num_heads, seq_len2, hidden_size)
-    k2 = torch.rand(batch_size, num_heads, seq_len2, hidden_size)
-    v2 = torch.rand(batch_size, num_heads, seq_len2, hidden_size)
-    q2[:, :, :seq_len] = q
-    k2[:, :, :seq_len] = k
-    v2[:, :, :seq_len] = v
-    attention_mask2 = torch.ones(batch_size, 1, seq_len2, seq_len2)
-    attention_mask2[:, :, seq_len:, seq_len:] = 0
-
-    special_embeddings_mask2 = torch.zeros(batch_size, seq_len2, dtype=torch.long)
-    special_embeddings_mask2[:, :seq_len] = special_embeddings_mask
-    clothest_end_of_sentence_token_idx2 = special_token_mask_to_clothest_token_idx_slow(special_embeddings_mask2)
-
-    output2, _ = sentence_attention_forward(
-        None,
-        q2, k2, v2,
-        attention_mask2,
-        scaling=scaling,
-        clothest_end_of_sentence_token_idx=clothest_end_of_sentence_token_idx2,
-        special_embeddings_mask=special_embeddings_mask2,
-    )
-
-    assert torch.allclose(output1, output2[:, :, :seq_len, :]), "output1 and output2 are not equal"
-
-
-def test_sentence_attention_causal_mask():
-
-    hidden_size = 128
-    batch_size = 4
-    seq_len = 7
-    num_heads = 8
-
-    scaling = 1.0 / hidden_size ** 0.5
-
-    q = torch.rand(batch_size, num_heads, seq_len, hidden_size)
-    k = torch.rand(batch_size, num_heads, seq_len, hidden_size)
-    v = torch.rand(batch_size, num_heads, seq_len, hidden_size)
-    attention_mask = torch.ones(batch_size, seq_len)
-
-    special_embeddings_mask = torch.zeros(batch_size, seq_len, dtype=torch.long)
-    special_embeddings_mask[:, 2] = 1
-    special_embeddings_mask[:, 5] = 1
-    clothest_end_of_sentence_token_idx = special_token_mask_to_clothest_token_idx_slow(special_embeddings_mask)
-
-    output1, _ = sentence_attention_forward(
-        None,
-        q, k, v,
-        attention_mask,
-        scaling=scaling,
-        clothest_end_of_sentence_token_idx=clothest_end_of_sentence_token_idx,
-        special_embeddings_mask=special_embeddings_mask,
-    )
-
-    seq_len2 = seq_len * 2
-    q2 = torch.rand(batch_size, num_heads, seq_len2, hidden_size)
-    k2 = torch.rand(batch_size, num_heads, seq_len2, hidden_size)
-    v2 = torch.rand(batch_size, num_heads, seq_len2, hidden_size)
-    q2[:, :, :seq_len] = q
-    k2[:, :, :seq_len] = k
-    v2[:, :, :seq_len] = v
-    attention_mask2 = torch.ones(batch_size, seq_len2)
-
-    special_embeddings_mask2 = torch.zeros(batch_size, seq_len2, dtype=torch.long)
-    special_embeddings_mask2[:, :seq_len] = special_embeddings_mask
-    clothest_end_of_sentence_token_idx2 = special_token_mask_to_clothest_token_idx_slow(special_embeddings_mask2)
-
-    output2, _ = sentence_attention_forward(
-        None,
-        q2, k2, v2,
-        attention_mask2,
-        scaling=scaling,
-        clothest_end_of_sentence_token_idx=clothest_end_of_sentence_token_idx2,
-        special_embeddings_mask=special_embeddings_mask2,
-    )
-
-    assert torch.allclose(output1, output2[:, :, :seq_len, :]), "output1 and output2 are not equal"
 
 
 if __name__ == "__main__":
--- tests/tokenized_fineweb_test.py
+++ tests/tokenized_fineweb_test.py
@@ -2,12 +2,43 @@
 from datasets import Dataset
 from transformers import AutoTokenizer, GPT2TokenizerFast
 from transformers.models.gpt2.tokenization_gpt2_fast import GPT2TokenizerFastEOS
+import shutil
 
+def _test_tokenized_fineweb(tokenizer):
+    dataset = Dataset.load_from_disk("artifacts/data/fineweb_edu_tokenized_SmolLM2-1.7B_with_eos_token/shard_0/")
+    item = dataset[0]
+    decoded = tokenizer.decode(item['input_ids'])
+    assert decoded.count('<end_of_sentence>') == 32, 'decoded content has 32 eos tokens'
 
 def test_tokenized_fineweb():
-    dataset = Dataset.load_from_disk("fineweb_edu_tokenized_gpt2_eos/000_00000.parquet/")
-    item = dataset[0]
     tokenizer = GPT2TokenizerFastEOS.from_pretrained("HuggingFaceTB/SmolLM2-1.7B")
+    _test_tokenized_fineweb(tokenizer)
+
+    shutil.rmtree("/tmp/eos_tokenizer", ignore_errors=True)
+    tokenizer_path = "/tmp/eos_tokenizer"
+    tokenizer.save_pretrained(tokenizer_path)
+    tokenizer_loaded = GPT2TokenizerFastEOS.from_pretrained(tokenizer_path)
+    assert tokenizer_loaded.num_eos_tokens == 1, 'reloaded tokenizer has 1 eos token'
+    _test_tokenized_fineweb(tokenizer_loaded)
+
+def _test_tokenized_fineweb_4_eos_tokens(tokenizer):
+    dataset = Dataset.load_from_disk("artifacts/data/fineweb_edu_tokenized_SmolLM2-1.7B_with_eos_token_num_4/shard_0/")
+    item = dataset[0]
+
     decoded = tokenizer.decode(item['input_ids'])
-    print("decoded", decoded)
-    breakpoint()
+    assert decoded.count('<end_of_sentence_') == 32 * 4, 'decoded content has 32 eos tokens'
+
+    for i in range(4):
+        assert decoded.count(f'<end_of_sentence_{i}>') == 32, f'decoded content has 32 eos tokens for {i}'
+
+def test_tokenized_fineweb_4_eos_tokens():
+    tokenizer = GPT2TokenizerFastEOS.from_pretrained("HuggingFaceTB/SmolLM2-1.7B", num_eos_tokens=4)
+    _test_tokenized_fineweb_4_eos_tokens(tokenizer)
+
+    shutil.rmtree("/tmp/eos_tokenizer_4", ignore_errors=True)
+    tokenizer_path = "/tmp/eos_tokenizer_4"
+    tokenizer.save_pretrained(tokenizer_path)
+    tokenizer_loaded = GPT2TokenizerFastEOS.from_pretrained(tokenizer_path)
+    assert tokenizer_loaded.num_eos_tokens == 4, 'reloaded tokenizer has 4 eos tokens'
+    _test_tokenized_fineweb_4_eos_tokens(tokenizer_loaded)
+
