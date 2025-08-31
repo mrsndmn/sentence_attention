b2ad3a20c5f5feaca30179547c83e7aa256e8035
Dmitrii Tarasov <d.tarasov@airi.net>
2025-08-31 09:05:32 +0300

    helmet eval files

--- src/sentence_attention/evaluation/benchmarks.py
+++ src/sentence_attention/evaluation/benchmarks.py
@@ -1,3 +1,4 @@
+import glob
 import os
 
 all_benchmarks = [
@@ -30,6 +31,15 @@ long_benchmarks = [
 
 
 def checkpoint_evaluation_file(model_checkpoint, task_name):
+
+    if task_name in long_benchmarks:
+        score_glob = os.path.join(model_checkpoint, "helmet_eval", task_name, "*.score")
+        score_files = glob.glob(score_glob)
+        if len(score_files) == 0:
+            return score_glob
+
+        return score_files[0]
+
     evaluation_dir = os.path.join(model_checkpoint, "evaluation")
     os.makedirs(evaluation_dir, exist_ok=True)
     return os.path.join(evaluation_dir, f"{task_name}.json")
